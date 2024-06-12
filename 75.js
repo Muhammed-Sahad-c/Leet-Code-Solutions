29a61d09a53aa830883b9bccb2e2d64394fb8e43
Muhammed Sahad <109616941+Muhammed-Sahad-c@users.noreply.github.com>
2024-06-12 14:27:14 +0530

    Sort Colors

--- 75.js
+++ 75.js
@@ -3,5 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    return nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                nums[j] += nums[i];
+                nums[i] = nums[j] - nums[i];
+                nums[j] -= nums[i];
+           a }
+        }
+    }
 };
